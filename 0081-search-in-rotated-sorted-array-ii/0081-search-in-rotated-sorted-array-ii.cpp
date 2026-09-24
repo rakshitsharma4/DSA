8f988691aebf8bce84c5dac4306050099f6c3c35
rakshitsharma4 <165375557+rakshitsharma4@users.noreply.github.com>
2026-09-24 17:15:04 +0530

    Time: 0 ms (100%), Space: 18.1 MB (46.08%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,36 +1,48 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int left = 0;
-        int right = nums.size() - 1;
-        
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            
+
+        int st = 0;
+        int end = nums.size() - 1;
+
+        while (st <= end) {
+
+            int mid = st + (end - st) / 2;
+
             if (nums[mid] == target) {
                 return true;
             }
-            
-            if (nums[mid] == nums[left]) {
-                left++;
+
+            // Corner case: duplicates
+            if (nums[st] == nums[mid] && nums[mid] == nums[end]) {
+                st++;
+                end--;
                 continue;
             }
-            
-            if (nums[left] <= nums[mid]) {
-                if (nums[left] <= target && target < nums[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
+
+            // Left half is sorted
+            if (nums[st] <= nums[mid]) {
+
+                if (nums[st] <= target && target < nums[mid]) {
+                    end = mid - 1;
                 }
-            } else {
-                if (nums[mid] < target && target <= nums[right]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
+                else {
+                    st = mid + 1;
+                }
+            }
+
+            // Right half is sorted
+            else {
+
+                if (nums[mid] < target && target <= nums[end]) {
+                    st = mid + 1;
+                }
+                else {
+                    end = mid - 1;
                 }
             }
         }
-        
-        return false;        
+
+        return false;
     }
 };
